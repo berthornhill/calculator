51e37cd64cee7fe1efa58a6aefa75b59a4d87d63
berthornhill <bert.hill72@gmail.com>
2020-09-28 16:21:21 -0400

    connected  operator buttons and execute buttons with JavaScript. Performing simple calculations succesfully. Issue with continuous calculaton.

--- javascript.js
+++ javascript.js
@@ -4,25 +4,29 @@
 
 // Addition
 function addition(numOne, numTwo) {
-  return numOne + numTwo;
+  return +numOne + +numTwo;
 }
 
 // Subtraction
 function subtraction(numOne, numTwo) {
-  return numOne - numTwo;
+  return +numOne - +numTwo;
 }
 
 // Multiplication
 function multiplication(numOne, numTwo) {
-  return numOne * numTwo;
+  return +numOne * +numTwo;
 }
 
 // Division
 function division(numOne, numTwo) {
-  return numOne / numTwo;
+  return +numOne / +numTwo;
 }
 
 // "operate()" to take in 2 numbers and call the appropriate mathmatical operator function.
+
+let numOne = "";
+let numTwo = "";
+let operator = "";
 
 function operate(numOne, numTwo, operator) {
   return operator == "+"
@@ -49,42 +53,101 @@ const executableButtons = executables.querySelectorAll("button");
 
 displayField.innerText = "";
 
-// Listener for button press
+// Listener for button press and displaying of number input.
+// if displaying operator, stores operator in variable.
+//-- sets variable hasNumberInput. this variable will be switched between true/flase for storing operator variables and clearing display without clearing required variables.
+let hasNumberInput = false;
+
 numbersButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    displayField.innerText += btn.dataset.value;
+    if (hasNumberInput == true) {
+      displayField.innerText = btn.dataset.value;
+      hasNumberInput = false;
+    } else {
+      displayField.innerText += btn.dataset.value;
+    }
   });
 });
 
+// When clicked, operator buttons should store previously input number string ("displayField.innerText")
+// as a variable. If another operator button is used before executing function, the most recent operator
+// clicked will be displayed.
+
 operatorButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    displayField.innerText += btn.dataset.value;
+    if (numOne == "") {
+      numOne = displayField.innerText; // 1
+      displayField.innerText = btn.dataset.value; // +
+      operator = btn.dataset.value;
+    } else if (numTwo == "") {
+      numTwo = displayField.innerText;
+      displayField.innerText = btn.dataset.value;
+      operator = btn.dataset.value;
+    } else {
+      numTwo = displayField.innerText;
+      displayField.innerText = btn.dataset.value;
+      operator = btn.dataset.value;
+      // numOne = operate(numOne, numTwo, operator);
+      equals();
+    }
+    hasNumberInput = true; // true
   });
 });
 
 executableButtons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    displayField.innerText += "We have to see the Great Deku";
+    if (btn.dataset.value == "CLEAR") {
+      clearButton();
+    } else if (btn.dataset.value == "BACKSPACE") {
+      backspace();
+    } else {
+      numTwo = displayField.innerText;
+      equals();
+      hasNumberInput = true;
+    }
   });
 });
 
-// Display for button press.
+// "CLEAR" button function. Resets "displayField.innerText" to empty str.
+function clearButton() {
+  displayField.innerText = "";
+  numOne = "";
+  numTwo = "";
+}
+
+// "BACKSPACE" button function. Removes last num/char in "displayField.innerText"
+function backspace() {
+  displayField.innerText = displayField.innerText.substring(
+    0,
+    displayField.innerText.length - 1
+  );
+}
+
+// EQUALS/EXECUTE button function
+
+function equals() {
+  if (numOne != "" || numTwo != "") {
+    numTwo = displayField.innerText;
+    numOne = operate(numOne, numTwo, operator);
+    displayField.innerText = numOne;
+    numTwo = "";
+  }
+}
 
 // Button Objects Key/Values
-
-const buttonPress = {
-  1: 1,
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  0: 0,
-  "+": "+",
-  "-": "-",
-  "*": "*",
-  "/": "/",
-};
+// const buttonPress = {
+//   1: 1,
+//   2: 2,
+//   3: 3,
+//   4: 4,
+//   5: 5,
+//   6: 6,
+//   7: 7,
+//   8: 8,
+//   9: 9,
+//   0: 0,
+//   "+": "+",
+//   "-": "-",
+//   "*": "*",
+//   "/": "/",
+// };
